3c6bfac9a52c3442092e66d031c258f4b0797e2d
Caio Nogueira <programmercaio98@gmail.com>
2024-06-01 18:59:14 -0300

    feat: alterado o bannerCarousel

--- islands/CustomSlider.tsx
+++ islands/CustomSlider.tsx
@@ -1,112 +1,253 @@
-import Swiper from "https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs";
-import { ImageWidget } from "apps/admin/widgets.ts";
+import {
+  SendEventOnClick,
+  SendEventOnView,
+} from "$store/components/Analytics.tsx";
+// deno-lint-ignore no-unused-vars
+import Button from "$store/components/ui/Button.tsx";
+import Icon from "$store/components/ui/Icon.tsx";
+import Slider from "$store/components/ui/Slider.tsx";
+import SliderJS from "$store/islands/SliderJS.tsx";
+import { useId } from "$store/sdk/useId.ts";
+import type { ImageWidget } from "apps/admin/widgets.ts";
+import { Picture } from "apps/website/components/Picture.tsx";
+// import Image from "apps/website/components/Image.tsx";
 
-export interface Image {
-  image: ImageWidget;
-  link?: string;
-  alt?: string;
-}
-
-interface Props {
-  images: Image[];
-  layout: {
-    slidesPerViewDesktop?: number;
-    slidesPerViewTablet?: number;
-    slidesPerViewMobile?: number;
-    spaceBetween?: number;
-    autoplay?: boolean;
-    showArrows?: boolean;
-    fundoDasArrows?: string;
+/**
+ * @titleBy alt
+ */
+export interface Banner {
+  /** @description desktop otimized image */
+  desktop: ImageWidget;
+  /** @description Image's alt text */
+  alt: string;
+  action?: {
+    /** @description when user clicks on the image, go to this link */
+    href: string;
+    /** @description Image text title */
+    title: string;
+    /** @description Image text subtitle */
+    subTitle: string;
+    /** @description Button label */
+    label: string;
   };
 }
 
-function CustomSlider({ images, layout }: Props) {
-  const swiper = new Swiper(".swiper", {
-    direction: "horizontal",
-    loop: true,
-    spaceBetween: layout.spaceBetween || 20,
-    autoplay: layout.autoplay,
+export interface Columns {
+  mobile?: 1 | 2;
+  desktop?: 1 | 2 | 3 | 4 | 5;
+}
 
-    // Navigation arrows
-    navigation: {
-      enabled: true,
-      nextEl: ".button-next",
-      prevEl: ".button-prev",
-    },
-    breakpoints: {
-      // when window width is >= 0
-      0: {
-        slidesPerView: layout.slidesPerViewMobile || 2,
-      },
-      768: {
-        slidesPerView: layout.slidesPerViewTablet || 3,
-      },
-      1024: {
-        slidesPerView: layout.slidesPerViewDesktop || 5,
-      },
-    },
-  });
+export interface Props {
+  images?: Banner[];
+  /**
+   * @description Check this option when this banner is the biggest image on the screen for image optimizations
+   */
+  preload?: boolean;
+  /**
+   * @title IN Container?
+   * @description Colocar num container menor?
+   */
+  container?: boolean;
+  /**
+   * @title Show arrows
+   * @description show arrows to navigate through the images
+   */
+  arrows?: boolean;
+  /**
+   * @title Show dots
+   * @description show dots to navigate through the images
+   */
+  dots?: boolean;
+  /**
+   * @title Autoplay interval
+   * @description time (in seconds) to start the carousel autoplay
+   */
+  interval?: number;
+  layout?: {
+    columns?: Columns;
+  };
+}
 
-  swiper;
+const MOBILE_COLUMNS = {
+  1: "w-full",
+  2: "w-1/2",
+};
+
+const DESKTOP_COLUMNS = {
+  1: "md:w-full",
+  2: "md:w-1/2",
+  3: "md:w-1/3",
+  4: "md:w-1/4",
+  5: "md:w-1/5",
+};
+
+const DEFAULT_PROPS = {
+  images: [
+    {
+      alt: "/feminino",
+      action: {
+        title: "New collection",
+        subTitle: "Main title",
+        label: "Explore collection",
+        href: "/",
+      },
+      desktop:
+        "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/2291/d057fc10-5616-4f12-8d4c-201bb47a81f5",
+    },
+    {
+      alt: "/feminino",
+      action: {
+        title: "New collection",
+        subTitle: "Main title",
+        label: "Explore collection",
+        href: "/",
+      },
+      desktop:
+        "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/2291/d057fc10-5616-4f12-8d4c-201bb47a81f5",
+    },
+    {
+      alt: "/feminino",
+      action: {
+        title: "New collection",
+        subTitle: "Main title",
+        label: "Explore collection",
+        href: "/",
+      },
+      desktop:
+        "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/2291/d057fc10-5616-4f12-8d4c-201bb47a81f5",
+    },
+  ],
+  preload: true,
+};
+
+function BannerItem(
+  { image, lcp, id }: { image: Banner; lcp?: boolean; id: string },
+) {
+  const {
+    alt,
+    desktop,
+    action,
+  } = image;
 
   return (
-    <>
-      <div class="container">
-        <div class="swiper">
-          <div class="swiper-wrapper">
-            {images.length > 0 &&
-              images.map((image) => (
-                <div class="swiper-slide">
-                  TESTE DE LOCALHOST
-                  <a href={image.link}>
-                    <img src={image.image} alt={image.alt} />
-                  </a>
-                </div>
-              ))}
-          </div>
+    <a
+      id={id}
+      href={action?.href ?? "#"}
+      aria-label={action?.label}
+      class="relative overflow-y-hidden w-full"
+    >
+      <Picture preload={lcp}>
+        <img src={desktop} alt={alt} loading={lcp ? "eager" : "lazy"} />
+      </Picture>
+    </a>
+  );
+}
 
-          <div
-            style={{
-              backgroundColor: layout.fundoDasArrows
-                ? layout.fundoDasArrows
-                : "",
-            }}
-            class={`${
-              layout.showArrows ? "" : "hidden"
-            } "button-prev absolute top-1/2 left-0 z-20 bg-white cursor-pointer"`}
-          >
-            <svg
-              width="24"
-              height="24"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M12 16a1 1 0 0 1-.707-.293l-5-5a1 1 0 0 1 0-1.414l5-5a.999.999 0 1 1 1.414 1.414L8.414 10l4.293 4.293A.999.999 0 0 1 12 16" />
-            </svg>
-          </div>
-          <div
-            style={{
-              backgroundColor: layout.fundoDasArrows
-                ? layout.fundoDasArrows
-                : "",
-            }}
-            class={`${
-              layout.showArrows ? "" : "hidden"
-            } "button-next  absolute top-1/2 right-0 z-20 bg-white cursor-pointer"`}
-          >
-            <svg
-              width="24"
-              height="24"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-              transform="rotate(180)"
-            >
-              <path d="M12 16a1 1 0 0 1-.707-.293l-5-5a1 1 0 0 1 0-1.414l5-5a.999.999 0 1 1 1.414 1.414L8.414 10l4.293 4.293A.999.999 0 0 1 12 16" />
-            </svg>
-          </div>
-        </div>
+function Dots({ images, interval = 0 }: Props) {
+  return (
+    <>
+      <style
+        dangerouslySetInnerHTML={{
+          __html: `
+          @property --dot-progress {
+            syntax: '<percentage>';
+            inherits: false;
+            initial-value: 0%;
+          }
+          `,
+        }}
+      />
+      <ul class="carousel justify-center col-span-full gap-6 z-10 row-start-4">
+        {images?.map((_, index) => (
+          <li class="carousel-item">
+            <Slider.Dot index={index}>
+              <div class="py-5">
+                <div
+                  class="w-[12px] h-[12px] rounded-full group-disabled:bg-black bg-white"
+                  style={{ animationDuration: `${interval}s` }}
+                />
+              </div>
+            </Slider.Dot>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+function Buttons() {
+  return (
+    <>
+      <div class="flex items-center justify-center z-10 col-start-1 row-start-2">
+        <Slider.PrevButton class="bg-transparent border-0">
+          <Icon
+            class="text-black"
+            size={24}
+            id="ChevronLeft"
+            strokeWidth={3}
+          />
+        </Slider.PrevButton>
+      </div>
+      <div class="flex items-center justify-center z-10 col-start-3 row-start-2">
+        <Slider.NextButton class="bg-transparent border-0">
+          <Icon
+            class="text-black"
+            size={24}
+            id="ChevronRight"
+            strokeWidth={3}
+          />
+        </Slider.NextButton>
       </div>
     </>
+  );
+}
+
+function CustomSlider(props: Props) {
+  const id = useId();
+  const { images, preload, interval } = { ...DEFAULT_PROPS, ...props };
+
+  const mobile = MOBILE_COLUMNS[props.layout?.columns?.mobile ?? 1];
+  const desktop = DESKTOP_COLUMNS[props.layout?.columns?.desktop ?? 1];
+
+  return (
+    <div
+      id={id}
+      class={`${
+        props.container ? "container" : ""
+      } grid grid-cols-[48px_1fr_48px] sm:grid-cols-[120px_1fr_120px] grid-rows-[1fr_48px_1fr_64px] sm:min-h-min mb-10`}
+    >
+      <Slider class="carousel carousel-center w-full col-span-full row-span-full">
+        {images?.map((image, index) => {
+          const params = { promotion_name: image.alt };
+          return (
+            <Slider.Item
+              index={index}
+              class={`carousel-item ${mobile} ${desktop}`}
+            >
+              <BannerItem
+                image={image}
+                lcp={index === 0 && preload}
+                id={`${id}::${index}`}
+              />
+              <SendEventOnClick
+                id={`${id}::${index}`}
+                event={{ name: "select_promotion", params }}
+              />
+              <SendEventOnView
+                id={`${id}::${index}`}
+                event={{ name: "view_promotion", params }}
+              />
+            </Slider.Item>
+          );
+        })}
+      </Slider>
+
+      {props.arrows && <Buttons />}
+
+      {props.dots && <Dots images={images} interval={interval} />}
+
+      <SliderJS rootId={id} interval={interval && interval * 1e3} infinite />
+    </div>
   );
 }
 
