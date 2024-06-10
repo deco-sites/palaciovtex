11bb5540ede6b3b7eb4f6510e552e6b1c934cdc3
Caio Nogueira <programmercaio98@gmail.com>
2024-06-10 13:35:52 -0300

    feat: adicionado skeleton no productCard

--- sections/Product/ProductCardCustom.tsx
+++ sections/Product/ProductCardCustom.tsx
@@ -1,1 +1,8 @@
 export { default } from "$store/components/product/ProductCardCustom.tsx";
+
+export function LoadingFallback() {
+    // Renderize spinners, esqueletos e outros espaços reservados
+    return (
+        <div class="skeleton w-full h-[570px] mb-3 bg-[#c2c2c2]"></div>
+    );
+}
