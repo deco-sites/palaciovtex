5a7ccddce6620f576f76132033e300f05590b699
Caio Nogueira <programmercaio98@gmail.com>
2024-06-26 14:00:17 -0300

    feat: inicio correção wishlist

--- components/wishlist/WishlistGallery.tsx
+++ components/wishlist/WishlistGallery.tsx
@@ -1,27 +1,65 @@
+// deno-lint-ignore-file
 import SearchResult, {
   Props as SearchResultProps,
 } from "$store/components/search/SearchResult.tsx";
+import { AppContext } from "deco/mod.ts";
 
 export type Props = SearchResultProps;
 
+
+async function loader(props: Props, req: Request, ctx: AppContext<any>) {
+
+  if (!props.page || !props.page.products || props.page.products.length === 0) {
+    return {
+      ...props,
+    };
+  }
+  
+  const products = await ctx.invoke.vtex.loaders.intelligentSearch.productList({
+    ids: props.page?.products.map((product) => product.productID),
+  });
+  
+
+  return {
+    ...props,
+    page: {
+      ...props.page,
+      products,
+    },
+    isWishlist: true,
+  };
+}
+
+export { loader };
+
 function WishlistGallery(props: Props) {
-  const isEmpty = !props.page || props.page.products.length === 0;
+  console.log(props)
+  const isEmpty = !props.page || !props.page.products ||
+    props.page.products.length === 0;
 
   if (isEmpty) {
     return (
-      <div class="container mx-4 sm:mx-auto">
+      <div class="md:container mx-4 sm:mx-auto">
         <div class="mx-10 my-20 flex flex-col gap-4 justify-center items-center">
-          <span class="font-medium text-2xl">Your wishlist is empty</span>
-          <span>
-            Log in and add items to your wishlist for later. They will show up
-            here
+          <span class="font-medium text-2xl text-center">
+            Sua Lista de Desejos está vazia
+          </span>
+          <span class="text-center">
+             Faça login e adicione itens à sua lista de desejos para visualizá-los mais tarde. Eles serão exibidos aqui.
           </span>
         </div>
       </div>
     );
   }
 
-  return <SearchResult {...props} />;
+  return (
+    <section class="container my-14 px-5 sm:px-0  flex flex-col justify-center items-center ">
+      <h1 class="text-xl font-bold lg:text-[32px] leading-8 lg:leading-1053 text-primary pb-10">
+        Minha Lista de Desejo
+      </h1>
+      <SearchResult {...props} />
+    </section>
+  );
 }
 
 export default WishlistGallery;
--- sections/Product/Wishlist.tsx
+++ sections/Product/Wishlist.tsx
@@ -1,1 +1,1 @@
-export { default } from "$store/components/wishlist/WishlistGallery.tsx";
+export { default, loader } from "$store/components/wishlist/WishlistGallery.tsx";
