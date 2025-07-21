0ac3d24f65d65d612dc1a688d39848ad566981b8
Tecnologia DNA Comunicação <tecnologia@dnacomunicacao.com>
2025-07-21 19:56:22 +0000

    Publish 1 from staging into origin/main

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -50,6 +50,22 @@ export default defineApp(async (_req, ctx) => {
         type="module"
         dangerouslySetInnerHTML={{ __html: `(${sw})();` }}
       />
+      <script>
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '935881335124841');
+          fbq('track', 'PageView');
+      </script>
+      <noscript>
+        <img height="1" width="1" style="display:none"
+        src="https://www.facebook.com/tr?id=935881335124841&ev=PageView&noscript=1"/>
+      </noscript>
     </>
   );
 });
